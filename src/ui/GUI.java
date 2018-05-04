f7c080eb472cd9c6915902f73dc63812fa49ea94
boranorben <zhotkp@gmail.com>
2018-05-04 22:36:34 +0700

    rewrite code

--- src/ui/GUI.java
+++ src/ui/GUI.java
@@ -14,25 +14,22 @@ import javax.swing.JPanel;
 
 import game.Game;
 
-
 public class GUI {
 
 	private Game game;
-	private JFrame homeFrame;
-	private JFrame gameFrame;
+	private JFrame homeFrame, gameFrame;
 	private JLabel rollResult;
 
 	public GUI(Game game) {
 		this.game = game;
-		this.homeFrame = new JFrame();
-		this.gameFrame = new JFrame();
-		homeFrame.setTitle("Snake Ladder Game");
+		this.homeFrame = new JFrame("Snakes and Ladders Game");
+		this.gameFrame = new JFrame("Snakes and Ladders Game");
 		homeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		initComponent();
 	}
 
-	public void initComponent() {		
-		JLabel boardPic = new JLabel(new ImageIcon(getClass().getResource("./../images/boardPic.jpg")));	
+	public void initComponent() {
+		JLabel boardPic = new JLabel(new ImageIcon(getClass().getResource("./../images/boardPic.jpg")));
 		JLabel coverPic = new JLabel(new ImageIcon(getClass().getResource("./../images/coverPic2.jpg")));
 		JButton player2_Button = new JButton(), player3_Button = new JButton(), player4_Button = new JButton();
 		JButton die = new JButton();
@@ -43,23 +40,23 @@ public class GUI {
 		JLabel currentPlayer = new JLabel("Current player");
 		rollResult = new JLabel("You Get: 0");
 
-		player2_Button.setIcon(new ImageIcon(getClass().getResource("./../images/2Player.png")));	
+		player2_Button.setIcon(new ImageIcon(getClass().getResource("./../images/2Player.png")));
 		player3_Button.setIcon(new ImageIcon(getClass().getResource("./../images/3Player.png")));
 		player4_Button.setIcon(new ImageIcon(getClass().getResource("./../images/4Player.png")));
 		die.setIcon(new ImageIcon(getClass().getResource("./../images/die.png")));
-		restartButton.setIcon(new ImageIcon(getClass().getResource("./../images/restart.png")));	
+		restartButton.setIcon(new ImageIcon(getClass().getResource("./../images/restart.png")));
 		replayButton.setIcon(new ImageIcon(getClass().getResource("./../images/replay.png")));
 
-		transparentBackground(player2_Button);
-		transparentBackground(player3_Button);
-		transparentBackground(player4_Button);
-		transparentBackground(die);
-		transparentBackground(restartButton);
-		transparentBackground(replayButton);
+		setTransparentBackground(player2_Button);
+		setTransparentBackground(player3_Button);
+		setTransparentBackground(player4_Button);
+		setTransparentBackground(die);
+		setTransparentBackground(restartButton);
+		setTransparentBackground(replayButton);
 
 		JPanel panelHome = new JPanel();
 		panelHome.add(coverPic);
-		coverPic.setLayout(new BoxLayout(coverPic,BoxLayout.PAGE_AXIS));
+		coverPic.setLayout(new BoxLayout(coverPic, BoxLayout.PAGE_AXIS));
 		coverPic.add(player2_Button);
 		coverPic.add(player3_Button);
 		coverPic.add(player4_Button);
@@ -78,92 +75,83 @@ public class GUI {
 		panelController.add(die);
 		panelController.add(restartButton);
 		panelController.add(replayButton);
-		panelController.setLayout(new BoxLayout(panelController,BoxLayout.PAGE_AXIS));
+		panelController.setLayout(new BoxLayout(panelController, BoxLayout.PAGE_AXIS));
 
 		homeFrame.add(panelHome, BorderLayout.NORTH);
 		gameFrame.add(panelBoard, BorderLayout.CENTER);
 		gameFrame.add(panelTitle, BorderLayout.NORTH);
 		gameFrame.add(panelController, BorderLayout.WEST);
 
-		ActionListener player2Listener = new ActionListener() {
+		player2_Button.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				homeFrame.setVisible(false);
-				gameFrame.setVisible(true);	
-				game.initPlayers(2);
+				changeFrame(2);
 			}
-		};
+		});
 
-		ActionListener player3Listener = new ActionListener() {
+		player3_Button.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				homeFrame.setVisible(false);
-				gameFrame.setVisible(true);	
-				game.initPlayers(3);
-
+				changeFrame(3);
 			}
-		};
+		});
 
-		ActionListener player4Listener = new ActionListener() {
+		player4_Button.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				homeFrame.setVisible(false);
-				gameFrame.setVisible(true);	
-				game.initPlayers(4);
+				changeFrame(4);
 			}
-		};
-		player2_Button.addActionListener(player2Listener);
-		player3_Button.addActionListener(player3Listener);
-		player4_Button.addActionListener(player4Listener);
+		});
 
-		ActionListener rollListener = new ActionListener() {
+		die.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				rollResult.setText("You Get: " + game.currentPlayerRollDice());
 			}
-		};
+		});
 
-		die.addActionListener(rollListener);
-
-		ActionListener restart = new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-
-			}
-		};
-		restartButton.addActionListener(restart);
-
-		ActionListener replay = new ActionListener() {
+		restartButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 
 			}
-		};
-		replayButton.addActionListener(replay);
+		});
+
+		replayButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
 
 		homeFrame.pack();
 		gameFrame.pack();
 		gameFrame.setVisible(false);
 	}
 
-	public void transparentBackground(JButton button){
+	public void setTransparentBackground(JButton button) {
 		button.setOpaque(false);
 		button.setContentAreaFilled(false);
 		button.setBorderPainted(false);
 	}
 
-
-	public static void main(String[] args) {
-		new GUI(new Game()).homeFrame.setVisible(true);;
+	public void changeFrame(int numPlayers) {
+		homeFrame.setVisible(false);
+		gameFrame.setVisible(true);
+		game.initPlayers(numPlayers);
 	}
 
-
+	public static void main(String[] args) {
+		new GUI(new Game()).homeFrame.setVisible(true);
+		;
+	}
 
 }
